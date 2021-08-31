1e762c4567f524413e17ade45581252883e5c680
AakashMishra11 <aakash11mishra@gmail.com>
2021-09-01 00:05:04 +0530

    Part 3: Failing test case

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,17 +18,16 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        if(getCurrentTime().isAfter(openingTime) && getCurrentTime().isBefore(closingTime)){
+        if (getCurrentTime().isAfter(openingTime) && getCurrentTime().isBefore(closingTime)) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
 
-    public LocalTime getCurrentTime(){
-        return  LocalTime.now();
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
     }
 
     public List<Item> getMenu() {
@@ -36,19 +35,19 @@ public class Restaurant {
         //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
     }
 
-    private Item findItemByName(String itemName){
-        for(Item item: menu) {
-            if(item.getName().equals(itemName))
+    private Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
                 return item;
         }
         return null;
     }
 
     public void addToMenu(String name, int price) {
-        Item newItem = new Item(name,price);
+        Item newItem = new Item(name, price);
         menu.add(newItem);
     }
-    
+
     public void removeFromMenu(String itemName) throws itemNotFoundException {
 
         Item itemToBeRemoved = findItemByName(itemName);
@@ -57,17 +56,19 @@ public class Restaurant {
 
         menu.remove(itemToBeRemoved);
     }
-    public void displayDetails(){
-        System.out.println("Restaurant:"+ name + "\n"
-                +"Location:"+ location + "\n"
-                +"Opening time:"+ openingTime +"\n"
-                +"Closing time:"+ closingTime +"\n"
-                +"Menu:"+"\n"+getMenu());
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
 
     }
 
     public String getName() {
         return name;
     }
+
 
 }
